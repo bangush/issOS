2f407494da0e1800e517d774e4b194512133acde
Chris <webmaster@itssimple.se>
2015-03-31 22:27:52 +0200

    Added functionality to move the the window while pressing Alt and Shift

--- issOS/MainWindow.xaml.cs
+++ issOS/MainWindow.xaml.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace issOS
 {
@@ -26,10 +30,39 @@ namespace issOS
 			InitializeComponent();
 		}
 
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+			var hwnd = new WindowInteropHelper(this).Handle;
+			WindowsServices.SetWindowExTransparent(hwnd);
+		}
+
+		Thread modCheck;
+
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
+			modCheck = new Thread(CheckKeyBoard);
+			modCheck.SetApartmentState(ApartmentState.STA);
+			modCheck.Start();
 			ipBox.Text = Environment.UserName;
 			LoadIPAddress();
+		}
+
+		private void CheckKeyBoard()
+		{
+			while (true)
+			{
+				var modenabled = Keyboard.Modifiers != (ModifierKeys.Shift & ModifierKeys.Alt);
+				RunOnUiThread((Action)delegate
+				{
+					var hwnd = new WindowInteropHelper(this).Handle;
+					if (modenabled)
+						WindowsServices.makeNormal(hwnd);
+					else
+						WindowsServices.SetWindowExTransparent(hwnd);
+				});
+				Thread.Sleep(100);
+			}
 		}
 
 		private async Task LoadIPAddress()
@@ -43,10 +76,46 @@ namespace issOS
 
 		private void ipBox_MouseMove(object sender, MouseEventArgs e)
 		{
-			if (e.LeftButton == MouseButtonState.Pressed)
+			var modenabled = Keyboard.Modifiers != (ModifierKeys.Shift & ModifierKeys.Alt);
+
+			if (modenabled && e.LeftButton == MouseButtonState.Pressed)
 			{
 				mainWin.DragMove();
+				RotateWindowSpherical();
 			}
+		}
+
+		private void RotateWindowSpherical()
+		{
+		}
+
+		public object RunOnUiThread(Delegate method)
+		{
+			return Dispatcher.Invoke(DispatcherPriority.Normal, method);
+		}
+	}
+
+	public static class WindowsServices
+	{
+		const int WS_EX_TRANSPARENT = 0x00000020;
+		const int GWL_EXSTYLE = (-20);
+
+		[DllImport("user32.dll")]
+		static extern int GetWindowLong(IntPtr hwnd, int index);
+
+		[DllImport("user32.dll")]
+		static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+		public static void SetWindowExTransparent(IntPtr hwnd)
+		{
+			var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+			SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+		}
+
+		public static void makeNormal(IntPtr hwnd)
+		{
+			int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+			SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle & ~WS_EX_TRANSPARENT);
 		}
 	}
 }
